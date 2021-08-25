ef5d98942ffd4470188df4bbe8035f7d68a4fa75
ksj0818 <ksj071808@gmail.com>
2021-08-25 23:36:47 +0900

    Update app.js

--- learning/javascript/app.js
+++ learning/javascript/app.js
@@ -1,20 +1,18 @@
 const ajax = new XMLHttpRequest();
-
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 // https://api.hnpwa.com/v0/news/1.json이라는 하는 것에서 가져오는 데이터를 동기적으로 가져오겠다.
-ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false); 
+ajax.open('GET', NEWS_URL, false); // url은 바뀔 수 있음 바뀔 가능성이 있는 데이터는 따로 변수로 빼주는게 좋음
 ajax.send();  // 실제로 데이터 가져오려면 ajax가 제공한 send함수 호출
-// console.log(ajax.response);              // 가져온 데이터는 response라고 하는 값에 들어있음
-/**
- * - document.getElementById와 같이 HTML을 조작하는 도구처럼 서버라고 하는 네트워크 너머에 있는 데이터를 가져오는 도구도 제공이 됨, 바로 그 도구가 ajax.
--  자바스크립트 코드에서 다루기 쉽도록 Response에 있는 데이터를 Preview탭에 있는 것처럼 바꿔보기, Preview 탭에서 보여주는 것들을 객체라고 부름, 응답 값을 객체로 바꿔보기 
-  JSON 데이터를 객체로 바꾸는 도구는 JSON.parse, 자바스크립트가 기본으로 제공,
-  이 함수는 괄호 안에 입력으로 받은 JSON 데이터를 객체로 바꿔서 반환을 해줌
- */
+// console.log(ajax.response);  // 가져온 데이터는 response라고 하는 값에 들어있음
 
 const newsFeed = JSON.parse(ajax.response);
-// 백틱 사용
-document.getElementById('root').innerHTML = `<ul>
-  <li>${newsFeed[0].title}</li>
-  <li>${newsFeed[1].title}</li>
-  <li>${newsFeed[2].title}</li>
-</ul>`;
+// document라고 하는 도구가 HTML를 조작하는 데 필요한 모든 도구들을 제공함
+const ul = document.createElement('ul'); // ul 태그를 반환
+
+for (let i=0; i<10; i++) {
+  const li = document.createElement('li'); // li 태그를 반환
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);  //root 하위에 추가
