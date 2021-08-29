6b4cc7a7fed57ab617f44e4fbdeca13e778a6040
ksj0818 <ksj071808@gmail.com>
2021-08-29 21:45:56 +0900

    Update app.js

--- learning/javascript/app.js
+++ learning/javascript/app.js
@@ -3,6 +3,14 @@
  * 변수 여러 개를 담을 수 있는 그릇 구조물은 객체,
  * 코드를 묶을 수 있는 구조는 함수.
  */
+// 문자열만을 가지고 UI를 만드는 방식
+// 문자열을 가지고 HTML을 만드는 방법은 innerHTML이라고 하는 DOM API가 제공하는 속성
+// innerHTML속성은 문자열을 넣었는데 그 문자열 안에 HTML 태그가 포함되어 있으면 그거 자체를 DOM API로,
+// 즉, HTML로 실제로 변환하는 것이 자동으로 처리된다는 뜻 
+
+// document라고 하는 도구가 HTML를 조작하는 데 필요한 모든 도구들을 제공함
+
+
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
@@ -20,57 +28,58 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-// document라고 하는 도구가 HTML를 조작하는 데 필요한 모든 도구들을 제공함
-const ul = document.createElement('ul'); // ul 태그를 반환
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-// hashchange는 어디서 발생하나, window객체가 있음 , addEventListenere() (어떤 이벤트가 발생하는지 듣고 있는 리스너를 등록)
-// 함수를 주는 이유는 자바스크립트의 특징 중 하나가 함수 자체를 값으로 취급해서 이렇게 함수한테 전달을 할 수도 있고 함수 자체를 실행할 수도 있음
-window.addEventListener('hashchange', function (){
+// push() 배열 요소의 가장 마지막 요소에 새로 추가하는 메서드 
+newsList.push('<ul>');
+
+for (let i=0; i<10; i++) {
+  newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count}
+      </a>
+    </li>
+  `)
+}
+newsList.push('</ul>');
+
+// 하나의 문자열로 합치는 기능을 배열이 제공함. join()
+container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
   // location 객체는 브라우저가 기본적으로 제공해주는 객체인데 주소와 관련된 다양한 정보들을 제공해 줌
   // substr(): 내가 쓰고 싶은 위치 값만 지정해 주면 그 이후부터의 값까지 씀
-  const id = location.hash.substr(1);
-  
+  const id = location.hash.substr(1);   
   // 문자열 함수 중에 값을 대체해 주는 replace 함수가 있음
   const newsContent = getData(CONTENT_URL.replace('@id', id));  // CONTENT_URL의 @id를 id로 대체해야함
   const title = document.createElement('h1');
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
-
     <div>
       <a href="#">목록으로</a>
     </div>
   `;
-}); 
-
-const newsList = [];
-
-// push() 배열 요소의 가장 마지막 요소에 새로 추가하는 메서드 
-newsList.push('<ul>');
-
-for (let i=0; i<10; i++) {
-  const div = document.createElement('div');
-  // 문자열만을 가지고 UI를 만드는 방식
-  // 문자열을 가지고 HTML을 만드는 방법은 innerHTML이라고 하는 DOM API가 제공하는 속성
-  // innerHTML속성은 문자열을 넣었는데 그 문자열 안에 HTML 태그가 포함되어 있으면 그거 자체를 DOM API로,
-  // 즉, HTML로 실제로 변환하는 것이 자동으로 처리된다는 뜻 
-  div.innerHTML = `
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count}
-      </a>
-    </li>
-    `
-    
-    ul.appendChild(div.firstElementChild);
-/** 
-  a.href = `#${newsFeed[i].id}`;  
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;  // 괄호 열고 닫는 걸로 표현하려면 백틱을 이용해서 ${}
-*/
-  
-  // li.appendChild(a);
 }
 
-container.appendChild(ul);  //root 하위에 추가
-container.appendChild(content); 
+// 화면 전환 중계
+function router() { 
+  const routePath = location.hash;
+
+  // location.hash에 #이 들어있을 때는  빈값을 반환함
+  if (routePath === '') {
+    newsFeed();
+  } else { 
+    newsDetail();
+  }
+}
+
+// hashchange는 어디서 발생하나, window객체가 있음 , addEventListenere() (어떤 이벤트가 발생하는지 듣고 있는 리스너를 등록)
+// 함수를 주는 이유는 자바스크립트의 특징 중 하나가 함수 자체를 값으로 취급해서 이렇게 함수한테 전달을 할 수도 있고 함수 자체를 실행할 수도 있음
+window.addEventListener('hashchange', router); 
+
+router();
